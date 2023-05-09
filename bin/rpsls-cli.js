497d144f01f3257b6f6c1e8122a1ced834cbb804
Preethi K <89270251+pgkavade@users.noreply.github.com>
2023-05-09 11:24:16 -0400

    Update rpsls-cli.js

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -7,10 +7,8 @@ var args = minimist(process.argv.slice(2));
 
 const help = `Usage: node-rpsls [SHOT]
 Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
-
   -h, --help        display this help message and exit
   -r, --rules       display the rules and exit
-
 Examples:
   node-rpsls        Return JSON with single player RPSLS result.
                     e.g. {"player":"rock"}
@@ -30,9 +28,10 @@ const rules = `Rules for the Lizard-Spock Expansion of Rock Paper Scissors:
 - Rock CRUSHES Scissors`;
 
 if (args.h || args.help) {
-  console.log(help);
+	console.log(help);
 	process.exit(0);
 }
+
 if (args.r || args.rules) {
   console.log(rules);
 	process.exit(0);
@@ -47,4 +46,3 @@ try {
     console.log(help);
     console.log(rules);
 }
-
