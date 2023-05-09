68f9c33cea7c8872850e72bc6a3ec8ed1b982dda
Preethi K <89270251+pgkavade@users.noreply.github.com>
2023-05-09 11:20:21 -0400

    Update rpsls-cli.js

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import rpsls from '../lib/rpsls.js'
-import minimist from 'minimist'
+import { rpsls } from "../lib/rpsls.js";
+import minimist from 'minimist';
 
 const args = minimist(process.argv.slice(2));
 
