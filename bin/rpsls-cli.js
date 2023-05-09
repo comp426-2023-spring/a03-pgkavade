8e3a2840b64c7cbdea89fba85de697bb885fd1ff
Preethi K <89270251+pgkavade@users.noreply.github.com>
2023-05-08 23:27:30 -0400

    added files

---
+++ bin/rpsls-cli.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+import rpsls from '../lib/rpsls.js'
+import minimist from 'minimist'
+
+const args = minimist(process.argv.slice(2));
+
+const help = `Usage: node-rpsls [SHOT]
+Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
+
+  -h, --help        display this help message and exit
+  -r, --rules       display the rules and exit
+
+Examples:
+  node-rpsls        Return JSON with single player RPSLS result.
+                    e.g. {"player":"rock"}
+  node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
+                    e.g {"player":"rock","opponent":"Spock","result":"lose"}`;
+
+
+
